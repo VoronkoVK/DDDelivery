e14d5afa49c37467e451ea269bac55a7796acc2a
VoronkoVK <mail@voronko.dev>
2026-07-06 10:25:44 +0300

    Реализовал Location value object

---
+++ DeliveryApp.Core/Domain/Models/SharedKernel/Location.cs
@@ -0,0 +1,53 @@
+﻿using CSharpFunctionalExtensions;
+using Errs;
+
+namespace DeliveryApp.Core.Domain.Models.SharedKernel;
+
+public class Location : ValueObject
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Location()
+    {
+    }
+
+    private Location(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    
+    public static Result<Location, Error> Create(int x, int y)
+    {
+        if (x is < 0 or > 10)
+            return GeneralErrors.ValueMustBeBetween(nameof(x), x, 0, 10);
+        
+        if (y is < 0 or > 10)
+            return GeneralErrors.ValueMustBeBetween(nameof(y), y, 0, 10);
+
+        return new Location(x, y);
+    }
+
+    public static Location CreateRandom()
+    {
+        return new Location(Random.Shared.Next(0, 10), Random.Shared.Next(0, 11));
+    }
+
+    public Result<int, Error> DistanceTo(Location target)
+    {
+        if (target is null)
+            return GeneralErrors.ValueIsRequired(nameof(target));
+        
+        var x = Math.Abs(target.X - X);
+        var y = Math.Abs(target.Y - Y);
+
+        return x + y;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return X;
+        yield return Y;
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/Models/SharedKernel/LocationTests.cs
@@ -0,0 +1,97 @@
+﻿using DeliveryApp.Core.Domain.Models.SharedKernel;
+using FluentAssertions;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.Models.SharedKernel;
+
+public class LocationTests
+{
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(5, 5)]
+    [InlineData(10, 10)]
+    public void Create_ValidValue_ShouldCreateLocation(int x, int y)
+    {
+        //  Arrange
+        
+        //  Act
+        var location = Location.Create(x, y);
+        
+        //  Assert
+        location.IsSuccess.Should().BeTrue();
+        location.Value.X.Should().Be(x);
+        location.Value.Y.Should().Be(y);
+    }
+    
+    [Theory]
+    [InlineData(-3, -3)]
+    [InlineData(-5, 5)]
+    [InlineData(11, 11)]
+    public void Create_InvalidValue_ShouldFail(int x, int y)
+    {
+        //  Arrange
+        
+        //  Act
+        var location = Location.Create(x, y);
+        
+        //  Assert
+        location.IsFailure.Should().BeTrue();
+    }
+
+    [Fact]
+    public void CreateRandom_ShouldCreateRandomLocation()
+    {
+        //  Arrange
+        
+        //  Act
+        var location = Location.CreateRandom();
+        
+        //  Assert
+        location.Should().NotBeNull();
+        location.X.Should().BeInRange(0, 10);
+        location.Y.Should().BeInRange(0, 10);
+    }
+    
+    [Fact]
+    public void DistanceTo_WhenTargetIsNull_ShouldFail()
+    {
+        //  Arrange
+        
+        //  Act
+        var location = Location.CreateRandom();
+        var distance = location.DistanceTo(null);
+        
+        //  Assert
+        distance.IsFailure.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void DistanceTo_WithSameLocation_ShouldReturnZero()
+    {
+        //  Arrange
+        
+        //  Act
+        var location1 = Location.Create(4, 5).Value;
+        var location2 = Location.Create(4, 5).Value;
+        var distance = location1.DistanceTo(location2);
+        
+        //  Assert
+        distance.IsSuccess.Should().BeTrue();
+        distance.Value.Should().Be(0);
+    }
+
+    [Fact]
+    public void DistanceTo_WithDifferentLocation_ShouldReturnDistance()
+    {
+        //  Arrange
+        
+        //  Act
+        var location1 = Location.Create(2, 7).Value;
+        var location2 = Location.Create(8, 4).Value;
+        var distance = location1.DistanceTo(location2);
+        
+        //  Assert
+        distance.IsSuccess.Should().BeTrue();
+        distance.Value.Should().Be(9);
+    }
+}
