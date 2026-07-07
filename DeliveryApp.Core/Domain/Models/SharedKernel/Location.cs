4901a9f0147b20910f8edb58e923482ba0be91f9
VoronkoVK <mail@voronko.dev>
2026-07-07 09:51:57 +0300

    Вынес максимальное и минимальное значения в константы и исправил проверки

--- DeliveryApp.Core/Domain/Models/SharedKernel/Location.cs
+++ DeliveryApp.Core/Domain/Models/SharedKernel/Location.cs
@@ -5,10 +5,12 @@ namespace DeliveryApp.Core.Domain.Models.SharedKernel;
 
 public class Location : ValueObject
 {
+    public const int MinBoundary = 1;
+    public const int MaxBoundary = 10;
     public int X { get; }
     public int Y { get; }
 
-    public Location()
+    private Location()
     {
     }
 
@@ -17,28 +19,29 @@ public class Location : ValueObject
         X = x;
         Y = y;
     }
-    
+
     public static Result<Location, Error> Create(int x, int y)
     {
-        if (x is < 0 or > 10)
-            return GeneralErrors.ValueMustBeBetween(nameof(x), x, 0, 10);
-        
-        if (y is < 0 or > 10)
-            return GeneralErrors.ValueMustBeBetween(nameof(y), y, 0, 10);
+        if (x is < MinBoundary or > MaxBoundary)
+            return GeneralErrors.ValueMustBeBetween(nameof(x), x, MinBoundary, MaxBoundary);
+
+        if (y is < MinBoundary or > MaxBoundary)
+            return GeneralErrors.ValueMustBeBetween(nameof(y), y, MinBoundary, MaxBoundary);
 
         return new Location(x, y);
     }
 
     public static Location CreateRandom()
     {
-        return new Location(Random.Shared.Next(0, 10), Random.Shared.Next(0, 11));
+        return new Location(Random.Shared.Next(MinBoundary, MaxBoundary + 1),
+            Random.Shared.Next(MinBoundary, MaxBoundary + 1));
     }
 
     public Result<int, Error> DistanceTo(Location target)
     {
         if (target is null)
             return GeneralErrors.ValueIsRequired(nameof(target));
-        
+
         var x = Math.Abs(target.X - X);
         var y = Math.Abs(target.Y - Y);
 
