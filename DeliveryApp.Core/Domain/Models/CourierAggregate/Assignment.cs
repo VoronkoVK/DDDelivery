13e82cb3a657221b3e25d695127e5028c63b5a5d
VoronkoVK <mail@voronko.dev>
2026-07-11 16:06:41 +0300

    Реализовал сущность Assignment, и тесты для нее

---
+++ DeliveryApp.Core/Domain/Models/CourierAggregate/Assignment.cs
@@ -0,0 +1,57 @@
+﻿using CSharpFunctionalExtensions;
+using DeliveryApp.Core.Domain.Models.SharedKernel;
+using Errs;
+
+namespace DeliveryApp.Core.Domain.Models.CourierAggregate;
+
+public class Assignment : Entity<Guid>
+{
+    private Assignment()
+    {
+    }
+
+    private Assignment(Guid orderId, Volume volume, Location location, Status status)
+    {
+        Id = Guid.NewGuid();
+        OrderId = orderId;
+        Volume = volume;
+        Location = location;
+        Status = status;
+    }
+
+    public Guid OrderId { get; private set; }
+    public Volume Volume { get; private set; }
+    public Location Location { get; private set; }
+    public Status Status { get; private set; }
+
+    public static Assignment Create(Guid orderId, Volume volume, Location location)
+    {
+        return new Assignment(orderId, volume, location, Status.Assigned);
+    }
+
+    public Result<object, Error> Complete(Location courierLocation)
+    {
+        if (Status == Status.Completed)
+        {
+            return Errors.AssignmentAlreadyCompleted;
+        }
+
+        var distance = Location.DistanceTo(courierLocation);
+        if (distance.IsFailure || distance.Value > 1)
+        {
+            return Errors.AssignmentInvalidDistance;
+        }
+
+        Status = Status.Completed;
+        return new object();
+    }
+
+    public static class Errors
+    {
+        public static Error AssignmentAlreadyCompleted =>
+            new Error($"{nameof(Assignment).ToLowerInvariant()}.already.completed", "Assignment already completed");
+
+        public static Error AssignmentInvalidDistance =>
+            new Error($"{nameof(Assignment).ToLowerInvariant()}.invalid.distance", "Assignment invalid distance");
+    }
+}
